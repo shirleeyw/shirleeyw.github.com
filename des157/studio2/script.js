9f1e7230ef8935be04e00ed00b5a34554a96bbc4
Shirley Wong <shiwong@ucdavis.edu>
2021-02-10 16:38:59 -0800

    trying something for studio2

--- des157/studio2/script.js
+++ des157/studio2/script.js
@@ -5,14 +5,28 @@
     const myjournal = document.getElementById("journal");
     const pen = document.getElementById("pen");
     
-    myjournal.addEventListener("click", function(){
-        console.log(myjournal.src);
+    myjournal.addEventListener("click", function(event){
+        event.preventDefault();
 
-        if(myjournal.src =="https://shirleeyw.github.io/des157/studio2/images/closed.png"){
+        /* if(myjournal.src =="https://shirleeyw.github.io/des157/studio2/images/closed.png"){
             myjournal.src = "https://shirleeyw.github.io/des157/studio2/images/open.JPG";
             pen.setAttribute("class", "show");
+            myjournal.style.cursor = "pointer";
+            console.log(`pen appear`);
+    } */
+
+    function changeImage() {
+        if(document.getElementById('journal').src == "https://shirleeyw.github.io/des157/studio2/images/closed.png"){
+            document.getElementById('journal').src ="https://shirleeyw.github.io/des157/studio2/images/open.JPG";
+            pen.setAttribute("class", "show");
+            myjournal.style.cursor = "pointer";
         }
+        else {
+            document.getElementById('journal').src = "https://shirleeyw.github.io/des157/studio2/images/closed.png";
 
-    })
+        }
+    }
 
-})
+    });
+
+});
