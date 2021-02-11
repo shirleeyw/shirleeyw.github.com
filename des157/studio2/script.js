1014033726878fd194f8871e8d6a816be70c8348
Shirley Wong <shiwong@ucdavis.edu>
2021-02-10 16:53:15 -0800

    studio2 trying something

--- des157/studio2/script.js
+++ des157/studio2/script.js
@@ -5,8 +5,7 @@
     const myjournal = document.getElementById("journal");
     const pen = document.getElementById("pen");
     
-    myjournal.addEventListener("click", function(event){
-        event.preventDefault();
+    myjournal.addEventListener("click", function(){
         console.log(myjournal.src);
 
         if(myjournal.src =="https://shirleeyw.github.io/des157/studio2/images/closed.png"){
