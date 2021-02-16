3ce1e074358757745854f0eda78521716ea103f4
Shirley Wong <shiwong@ucdavis.edu>
2021-02-15 16:40:21 -0800

    studio2 + pt 2 of lj

--- des157/studio2/script.js
+++ des157/studio2/script.js
@@ -11,7 +11,7 @@
     const closeBtns = document.querySelectorAll('.close');
 
 
-    //open overlays------------------------------
+    //-----------------open overlays------------------
     
     brushpen.addEventListener('click', function(event){
         event.preventDefault();
@@ -39,8 +39,7 @@
     });
 
 
-
-    //closing overlays when 'x' button clicked
+    //------------closing overlays when 'x' button clicked----------
     for(const eachBtn of closeBtns){
         eachBtn.addEventListener('click', function(event){
             event.preventDefault();
@@ -52,44 +51,5 @@
         })
     }
 
-
-        /* description.innerHTML = `<p>Brush Pens <br> These brush pens are my go-to pens for doing fancy calligraphy. But they also are great for coloring in illustrations!`;
-        
-        document.getElementById('overlay').className = 'show';
-    });
-    
-    flags.addEventListener('click', function(event){
-        event.preventDefault();
-        description.innerHTML = `Sticky notes add pops of color to my journal. Sometimes when I need to quickly jot something down, I would write it on a sticky note and then insert it into my journal later.`;
-
-        document.getElementById('overlay').className = 'show';
-    });
-
-    journal.addEventListener('click', function(event){
-        event.preventDefault();
-        description.innerHTML = `This is where all my thoughts and school assignments call home.`;
-
-        document.getElementById('overlay').className = 'show';
-    });
-
-    pen.addEventListener('click', function(event){
-        event.preventDefault();
-        description.innerHTML = `Zebra Mildliners are literally the best highlighters out there because the colors are better than the bright neon ones you usually see. Plus, it is double sided!`;
-
-        document.getElementById('overlay').className = 'show';
-    });
-
-    tape.addEventListener('click', function(event){
-        event.preventDefault();
-        description.innerHTML = `Using washi tape is my favorite way to spice up my journal spreads. I currently have a washi tape collection that is getting out of control, but I still buy them anyways :)`;
-
-        document.getElementById('overlay').className = 'show';
-    });
-    
-
-    document.querySelector('.close').addEventListener('click', function(event){
-        event.preventDefault();
-        document.getElementById('overlay').className = 'hidden';
-    }); */
 
 }());
